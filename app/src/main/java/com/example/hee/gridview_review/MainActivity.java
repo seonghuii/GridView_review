c871c35e5ba3eebe72062d01ba9f523b8c4c84aa
seonghuii <31907639+seonghuii@users.noreply.github.com>
2017-10-08 18:43:24 +0900

    Initial commit

---
+++ app/src/main/java/com/example/hee/gridview_review/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.hee.gridview_review;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    //어댑터 생성하고 gridview에 연결
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this,
+                        "" + (position+1)+ "번째 선택",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }
+     //어댑터 정의
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+         private Integer[] mThumbIds = {
+                 R.drawable.sample_1, R.drawable.sample_2,
+                 R.drawable.sample_3, R.drawable.sample_4,
+                 R.drawable.sample_5, R.drawable.sample_6};
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position) {
+            return mThumbIds[position];
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(100, 100));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(8, 8, 8, 8);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            imageView.setImageResource(mThumbIds[position]);
+            return imageView;
+        }
+
+
+    }
+
+}
